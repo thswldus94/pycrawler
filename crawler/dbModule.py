2ccc23024714d72341391b4727e9c5d4d6be42e7
sonjy <thswldus1994@naver.com>
2019-07-03 10:46:54 +0900

    dbconfig remove

---
+++ crawler/dbModule.py
@@ -0,0 +1,22 @@
+import pymysql
+
+# mysql db connection
+class Mysql:
+    def __init__(self):
+        self.db = pymysql.connect(
+                    host='localhost',
+                    user='root',
+                    password='qoWlfkdlej94',
+                    db='sonjy',
+                    charset='utf8')
+        self.cursor = self.db.cursor()
+
+    def execute(self, sql, params = None):
+        self.cursor.execute(sql, params)
+
+    def query(self, sql, params = None):
+        self.cursor.execute(sql, params)
+        return self.cursor.fetchall()
+
+    def commit(self):
+        self.db.commit()
